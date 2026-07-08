cdd1bd44ff2690f1c38c46259c155c37112ed7e4
Dhruv Narayan Vishwakarma <dhruvnvishwa@gmail.com>
2026-07-08 23:38:05 +0530

    chore: add open-next.config.ts and OpenNext config for Cloudflare Pages

---
+++ open-next.config.ts
@@ -0,0 +1,10 @@
+import { type OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+    },
+  },
+};
+export default config;
